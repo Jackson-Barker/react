3c1fce79d068e3809851754fc81f9a5f101e014f
jbarker <jackson.barker@du.edu>
2021-12-20 20:04:27 -0700

    working react todo list

--- src/Todo.js
+++ src/Todo.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
-export default function Todo({ todo }) {
+export default function Todo({ todo, toggleTodo }) {
+    function handleTodoCLick() {
+        toggleTodo(todo.id)
+    }
     return (
         <div>
-            
+            <label>
+              <input type="checkbox" checked={todo.complete} onChange={handleTodoCLick} />  
+            {todo.name}
+            </label>
         </div>
     )
 }
